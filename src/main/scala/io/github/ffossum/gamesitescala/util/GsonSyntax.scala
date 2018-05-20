0cd68728223af6ce6bafae50856f461a9009c64c
Fredrik Fossum <fredrik.fossum@gmail.com>
2018-05-20 18:20:10 +0200

    add gsonToCirce function

--- src/main/scala/io/github/ffossum/gamesitescala/util/GsonSyntax.scala
+++ src/main/scala/io/github/ffossum/gamesitescala/util/GsonSyntax.scala
@@ -11,6 +11,12 @@ object GsonSyntax {
     def asGson: G.JsonElement = circeToGson(t)
   }
 
+  implicit class GsonToCirce(gson: G.JsonElement) {
+    def as[D: Decoder]: Decoder.Result[D] = {
+      gsonToCirce(gson).as[D]
+    }
+  }
+
   def circeToGson(json: Json): G.JsonElement = {
     json.fold[G.JsonElement](
       G.JsonNull.INSTANCE,
@@ -33,5 +39,33 @@ object GsonSyntax {
       }
     )
   }
+
+  def gsonToCirce(gson: G.JsonElement): Json = {
+    import scala.collection.JavaConverters._
+
+    if (gson.isJsonArray) {
+      val array  = gson.getAsJsonArray
+      val values = array.iterator().asScala.toList.map(gsonToCirce)
+      Json.arr(values: _*)
+    } else if (gson.isJsonObject) {
+      val obj    = gson.getAsJsonObject
+      val fields = obj.entrySet.asScala.toList.map(x => (x.getKey, gsonToCirce(x.getValue)))
+      Json.obj(fields: _*)
+
+    } else if (gson.isJsonPrimitive) {
+      val primitive = gson.getAsJsonPrimitive
+      if (primitive.isBoolean) {
+        Json.fromBoolean(primitive.getAsBoolean)
+      } else if (primitive.isString) {
+        Json.fromString(primitive.getAsString)
+      } else {
+        parser.parse(primitive.toString).right.get
+      }
+
+    } else {
+      Json.Null
+    }
+  }
+
   def circeToGson[T: Encoder](t: T): G.JsonElement = circeToGson(t.asJson)
 }
--- src/test/scala/io/github/ffossum/gamesitescala/util/GsonSyntaxTest.scala
+++ src/test/scala/io/github/ffossum/gamesitescala/util/GsonSyntaxTest.scala
@@ -1,5 +1,6 @@
 package io.github.ffossum.gamesitescala.util
 
+import io.circe.parser
 import io.circe.syntax._
 import org.scalatest.prop.Checkers
 import org.scalatest.{FunSuite, Matchers}
@@ -32,6 +33,14 @@ class GsonSyntaxTest extends FunSuite with Matchers with Checkers {
 
       gson.toString === json.toString
     }
+  }
+
+  test("gson to circe") {
+    val json  = parser.parse("""{"a":[10,3.14,3.00e8,null]}""").right.get
+    val gson  = circeToGson(json)
+    val json2 = gsonToCirce(gson)
+
+    json.noSpaces shouldBe """{"a":[10,3.14,3.00e8,null]}"""
   }
 
 }
